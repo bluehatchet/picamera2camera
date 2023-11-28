4c99bc06f20c29e669660d688ca56011a930fa13
Justin <146759190+bluehatchet@users.noreply.github.com>
2023-11-28 11:03:35 -0500

    Create pi_camera_https.py

---
+++ pi_camera_https.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+
+# Mostly copied from https://picamera.readthedocs.io/en/release-1.13/recipes2.html
+# Run this script, then point a web browser at http:<this-ip-address>:8000
+# Note: needs simplejpeg to be installed (pip3 install simplejpeg).
+
+import io
+import logging
+import socketserver
+import libcamera
+from http import server, ssl
+from threading import Condition
+
+from picamera2 import Picamera2
+from picamera2.encoders import JpegEncoder
+from picamera2.outputs import FileOutput
+
+
+PAGE = """\
+<html>
+<head>
+<title>CAM#3</title>
+</head>
+<body>
+<h1>Picamera2 MJPEG HTTP Stream </h1>
+<img src="stream.mjpg" width="1280" height="720" />
+</body>
+</html>
+"""
+
+
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
+
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
+
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+    server_side=True   # - Added
+    certfile='localhost.pem'   # - Added
+    ssl_version=ssl.PROTOCOL_TLS  # - Added
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_video_configuration(main={"size": (1280, 720)}))  ###DEMO SIZE: 640,480
+#picam2.set_controls({'HdrMode': libcamera.controls.HdrModeEnum.SingleExposure})
+
+output = StreamingOutput()
+picam2.start_recording(JpegEncoder(), FileOutput(output))
+
+
+try:
+    address = ('', 8000)
+    server = StreamingServer(address, StreamingHandler)
+    server.serve_forever()
+finally:
+    picam2.stop_recording()
